b7eef82dea3d2909879e155783556a8bf7c6b217
Jared De Blander <jared@ayrscott.com>
2021-11-03 18:43:36 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Coevice-width, initial-scale=1.0">
+    <title>Title Me</title>
+</head>
+<body>
+    <p>My body</p>
+</body>
+</html>
